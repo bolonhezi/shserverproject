e937acf9bf770611b23636ec28c6bb408288dba0
Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>
2023-02-19 22:46:07 -0400

    Create 115.lua

---
+++ DiosRoomFix/ServerSide/115.lua
@@ -0,0 +1,89 @@
+-- //////////////////////////////////////////////////////////////////////
+-- AI 115.Lua
+-- //////////////////////////////////////////////////////////////////////
+Mob = LuaMob(CMob)
+math.randomseed(os.time())
+
+bMobSay = 0
+bMobLoop = 0
+bMove = 0
+dwNextSayTime = 0
+
+function OnMoveEnd(dwTime)
+    Mob:LuaSetHoldPosion(1)
+    Mob:LuaDeleteMob(2471, 1, 0.0, 0.0)
+end
+
+function OnAttacked(dwTime, dwCharID)
+    if (bMobSay == 0) then
+        Mob:LuaSayByIndex(6401, 500.0)
+        bMobSay = bMobSay + 1
+        dwNextSayTime = dwTime + 5000
+    end
+    bMove = 1
+end
+
+function OnAttackable(dwTime, dwCharID)
+    if (bMobSay == 0) then
+        Mob:LuaSayByIndex(6401, 500.0)
+        bMobSay = bMobSay + 1
+        dwNextSayTime = dwTime + 5000
+    end
+    bMove = 1
+end
+
+function Init()
+    Mob:LuaSetUnBeatable(1)
+end
+
+function OnNormalReset(dwTime)
+
+end
+
+function OnDeath(dwTime, dwAttackedCount)
+
+end
+
+function OnReturnHome(dwTime, dwAttackedCount)
+
+end
+
+function WhileCombat(dwTime, dwHPPercent, dwAttackedCount)
+
+    if (dwNextSayTime >= 5000) and (bMobSay <= 1) then
+        Mob:LuaSayByIndex(6402, 500.0)
+        bMobSay = bMobSay + 1
+    end
+
+    if (bMobLoop >= 1) then
+        return
+    end
+
+    nRandom = math.random(6)
+
+    if (nRandom == 1) then
+        Mob:LuaCreateMob(2464, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2465, 1, 220.0, 180.0)
+    elseif (nRandom == 2) then
+        Mob:LuaCreateMob(2464, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2466, 1, 220.0, 180.0)
+    elseif (nRandom == 3) then
+        Mob:LuaCreateMob(2464, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2467, 1, 220.0, 180.0)
+    elseif (nRandom == 4) then
+        Mob:LuaCreateMob(2465, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2466, 1, 220.0, 180.0)
+    elseif (nRandom == 5) then
+        Mob:LuaCreateMob(2465, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2467, 1, 220.0, 180.0)
+    elseif (nRandom == 6) then
+        Mob:LuaCreateMob(2466, 1, 220.0, 180.0)
+        Mob:LuaCreateMob(2467, 1, 220.0, 180.0)
+    end
+    bMobLoop = bMobLoop + 1
+
+    if (bMove == 1) then
+        Mob:LuaFixedMove(1, 173, 179)
+        bMove = bMove + 1
+    end
+end
