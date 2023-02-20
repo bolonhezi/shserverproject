adfef92ef93924abed8301a8d8a847f1e364bb0f
Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>
2023-02-19 22:45:20 -0400

    Create 113.lua

---
+++ DiosRoomFix/ServerSide/113.lua
@@ -0,0 +1,36 @@
+-- //////////////////////////////////////////////////////////////////////
+-- AI 113.Lua
+-- //////////////////////////////////////////////////////////////////////
+Mob = LuaMob(CMob)
+
+function Init()
+
+end
+
+function OnAttacked(dwTime, dwCharID)
+
+end
+
+function OnAttackable(dwTime, dwCharID)
+
+end
+
+function OnNormalReset(dwTime)
+
+end
+
+function OnReturnHome(dwTime, dwAttackedCount)
+
+end
+
+function OnDeath(dwTime, dwAttackedCount)
+    Mob:LuaUpdateInsZonePortal(1001, 0)
+end
+
+function OnMoveEnd(dwTime)
+
+end
+
+function WhileCombat(dwTime, dwHPPercent, dwAttackedCount)
+
+end
