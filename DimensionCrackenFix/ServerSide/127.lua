8b802d47234cead0b8692f414f65e1e33c926e63
Jorgi Bolonhezi <luiz.bolonhezi.dias@gmail.com>
2023-02-19 23:48:11 -0400

    Update 127.lua

--- DimensionCrackenFix/ServerSide/127.lua
+++ DimensionCrackenFix/ServerSide/127.lua
@@ -25,7 +25,10 @@ function OnReturnHome(dwTime, dwAttackedCount)
 end
 
 function OnNormalReset(dwTime)
-
+    bMobSay = 0
+    bMobMove = 0
+    iclass = 0
+    iclass2 = 0
 end
 
 function OnDeath(dwTime, dwAttackedCount)
@@ -62,7 +65,7 @@ function WhileCombat(dwTime, dwHPPercent, dwAttackedCount)
     if (dwHPPercent <= 50) then
         if (bMobMove < 1) then
             while (iclass <= 5) do
-                Mob:LuaRecallUser(iclass, 20, 74)
+                Mob:LuaRecallUser(iclass, 20, 74, 380.78, 159.469, 510.01)
                 iclass = iclass + 1
             end
             bMobMove = bMobMove + 1
